0c13dbff01925edb7b5ba08d2a4b8aa5f9fe8816
Yassed Matta <yassed883@gmail.com>
2024-03-12 16:53:16 -0500

    Funcionalidad register

--- lib/Screens/Register.dart
+++ lib/Screens/Register.dart
@@ -1,68 +1,102 @@
 // ignore: file_names
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nutrisalud/Routes/AppRoutes.dart';
 import '../Helpers/HelpersExport.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../Providers/UsersProviders.dart';
 
 class Register extends StatefulWidget {
-  const Register({super.key});
+  const Register({Key? key}) : super(key: key);
 
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
 
   @override
   void dispose() {
-    _nameController.dispose();
-    _emailController.dispose();
+    _userNameController.dispose();
     _passwordController.dispose();
+    _nameController.dispose();
     super.dispose();
+  }
+
+  Future<bool> validarLogin() async {
+    bool accesoConcedido = false;
+
+    // Validar que el userName no esté siendo usado
+    List<Usuario> usuarios = await Usuario.getUsuarios();
+    for (var usuario in usuarios) {
+      if (usuario.usuario == _userNameController.text) {
+        print('Usuario ya existe');
+        return false;
+      }
+    }
+
+    // Crear el usuario
+    try {
+      Map<String, dynamic> nuevoUsuario = {
+        'nombre': _nameController.text,
+        'usuario': _userNameController.text,
+        'contrasena': _passwordController.text,
+      };
+
+      await Usuario.postUsuario(nuevoUsuario);
+      print('Usuario creado');
+      accesoConcedido = true;
+    } catch (e) {
+      print('Error al crear usuario: $e');
+      accesoConcedido = false;
+    }
+
+    return accesoConcedido;
   }
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: ColorsConstants.lightGreen,
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              AppBar(
-                backgroundColor: ColorsConstants.lightGreen,
-                foregroundColor: ColorsConstants.whiteColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            AppBar(
+              backgroundColor: ColorsConstants.lightGreen,
+              foregroundColor: ColorsConstants.whiteColor,
+            ),
+            // Logo
+            Container(
+              margin: EdgeInsets.only(top: screenHeight * 0.07),
+              child: SvgPicture.asset(
+                AssetsRoute.logoSvg,
+                color: ColorsConstants.whiteColor,
+                height: screenHeight * 0.17,
               ),
-              Container(
-                height: 200,
+            ),
+            const SizedBox(height: 30),
+            Expanded(
+              child: Container(
                 decoration: const BoxDecoration(
-                  color: ColorsConstants.lightGreen,
-                ),
-                child: Center(
-                  child: SvgPicture.asset(AssetsRoute.logoSvg,
-                      color: ColorsConstants.whiteColor,
-                      height: screenHeight * 0.17),
-                ),
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  color: ColorsConstants.whiteColor,
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+                    topRight: Radius.circular(50),
+                    topLeft: Radius.circular(50),
                   ),
+                  color: ColorsConstants.whiteColor,
                 ),
+                width: screenWidth * 1,
                 child: Padding(
-                  padding: const EdgeInsets.all(32.0),
+                  padding: const EdgeInsets.all(15.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
                       const SizedBox(height: 30),
                       const Text(
                         'Register',
@@ -73,7 +107,7 @@ class _RegisterState extends State<Register> {
                           color: ColorsConstants.darkGreen,
                         ),
                       ),
-                      const SizedBox(height: 40),
+                      const SizedBox(height: 16),
                       TextFormField(
                         controller: _nameController,
                         decoration: InputDecoration(
@@ -84,30 +118,18 @@ class _RegisterState extends State<Register> {
                           labelText: 'Name:',
                           labelStyle: const TextStyle(color: Colors.green),
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your name';
-                          }
-                          return null;
-                        },
                       ),
                       const SizedBox(height: 16),
                       TextFormField(
-                        controller: _emailController,
+                        controller: _userNameController,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(15.0),
                             borderSide: const BorderSide(color: Colors.green),
                           ),
-                          labelText: 'Email:',
+                          labelText: 'User Name:',
                           labelStyle: const TextStyle(color: Colors.green),
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          return null;
-                        },
                       ),
                       const SizedBox(height: 16),
                       TextFormField(
@@ -121,21 +143,184 @@ class _RegisterState extends State<Register> {
                           labelText: 'Password:',
                           labelStyle: const TextStyle(color: Colors.green),
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        },
                       ),
-                      const SizedBox(height: 24),
+                      const SizedBox(height: 12),
                       ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            //Crear post de registro de usuario
-                            //Si todo está bien mandará a screen Introduction
-                            Navigator.pushReplacementNamed(
-                                context, AppRoutes.introduction);
+                        onPressed: () async {
+                          // Aviso si el nombre esta vacio
+                          if (_nameController.text.isEmpty) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  title: const Text(
+                                    "The name is empty",
+                                    style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w600,
+                                      color: ColorsConstants.darkGreen,
+                                    ),
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            ColorsConstants.darkGreen,
+                                      ),
+                                      child: const Text(
+                                        "Close",
+                                        style: TextStyle(
+                                          fontFamily: 'Inter',
+                                          fontSize: 19,
+                                          fontWeight: FontWeight.w400,
+                                          color: ColorsConstants.whiteColor,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                            return;
+                          }
+
+                          // Aviso si el usuario esta vacio
+                          if (_userNameController.text.isEmpty) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  title: const Text(
+                                    "The user name is empty",
+                                    style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w600,
+                                      color: ColorsConstants.darkGreen,
+                                    ),
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            ColorsConstants.darkGreen,
+                                      ),
+                                      child: const Text(
+                                        "Close",
+                                        style: TextStyle(
+                                          fontFamily: 'Inter',
+                                          fontSize: 19,
+                                          fontWeight: FontWeight.w400,
+                                          color: ColorsConstants.whiteColor,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                            return;
+                          }
+
+                          // Aviso si la contraseña esta vacia
+                          if (_passwordController.text.isEmpty) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  title: const Text(
+                                    "The password is empty",
+                                    style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w600,
+                                      color: ColorsConstants.darkGreen,
+                                    ),
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            ColorsConstants.darkGreen,
+                                      ),
+                                      child: const Text(
+                                        "Close",
+                                        style: TextStyle(
+                                          fontFamily: 'Inter',
+                                          fontSize: 19,
+                                          fontWeight: FontWeight.w400,
+                                          color: ColorsConstants.whiteColor,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                            return;
+                          }
+
+                          if (await validarLogin()) {
+                            Navigator.pushNamed(context, AppRoutes.login);
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  title: const Text(
+                                    "The user name is already in use",
+                                    style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w600,
+                                      color: ColorsConstants.whiteColor,
+                                    ),
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            ColorsConstants.darkGreen,
+                                      ),
+                                      child: const Text(
+                                        "Close",
+                                        style: TextStyle(
+                                          fontFamily: 'Inter',
+                                          fontSize: 19,
+                                          fontWeight: FontWeight.w400,
+                                          color: ColorsConstants.whiteColor,
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
                           }
                         },
                         style: ElevatedButton.styleFrom(
@@ -146,38 +331,12 @@ class _RegisterState extends State<Register> {
                           style: TextStyle(color: ColorsConstants.whiteColor),
                         ),
                       ),
-                      const SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          IconButton(
-                            icon: const Icon(Icons.mail_outline),
-                            onPressed: () {
-                              // Acción para mail
-                            },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.loop),
-                            onPressed: () {
-                              // Acción para loop
-                            },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.biotech_rounded),
-                            onPressed: () {
-                              // Acción para Twitter
-                            },
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                          height: 20), // Espacio adicional en el fondo
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
